226a97232a80f275ab0917db9a94294d1e4585e5
anarkia1976 <stefano.villa1976@gmail.com>
2013-07-03 06:50:30 +0200

    fixmakoboot: Merge Values from Last Osmosis Settings - Thanks FTeam

--- ramdisk-4.2.2/sbin/fixmakoboot.sh
+++ ramdisk-4.2.2/sbin/fixmakoboot.sh
@@ -6,13 +6,12 @@
 
 bb=/sbin/bb/busybox;
 
-#
-# Disable mpDecision at boot
-#
+# Stop mpDecision at boot
 stop mpdecision
 
 $bb mount -o rw,remount /system;
 
+# create init.d folder
 if [ ! -d /system/etc/init.d ]; then
   $bb echo "Making Init.d Directory ...";
   $bb mkdir /system/etc/init.d;
@@ -23,6 +22,13 @@ else
 fi;
 
 $bb mount -o ro,remount /system;
+
+# disable sysctl.conf to prevent ROM interference
+if [ -e /system/etc/sysctl.conf ]; then
+  mount -o remount,rw /system;
+  mv /system/etc/sysctl.conf /system/etc/sysctl.conf.fkbak;
+  mount -o remount,ro /system;
+fi;
 
 # disable debugging
 echo "0" > /sys/module/wakelock/parameters/debug_mask;
@@ -42,28 +48,33 @@ for i in /sys/block/*/queue; do
   echo 0 > $i/rotational;
 done;
 
-# increase systemui process priority
-$bb sleep 10;
+# decrease dalvik vm heapgrowthlimit by altering the equation
+hs=`getprop dalvik.vm.heapsize | cut -dm -f1`;
+htu=`getprop dalvik.vm.heaptargetutilization`;
+hgl=`awk -v htu=$htu -v hs=$hs 'BEGIN { print (htu / 3) * hs }'`;
+setprop dalvik.vm.heapgrowthlimit $hgl'm';
 
-loopcount=1
-
-while [ $loopcount -le 18 ]; do
-if [ $($bb pgrep com.android.systemui) ] ; then
-    $bb renice -17 $($bb pgrep com.android.systemui);
-    break;
-else
-    (( loopcount++ ));
-    $bb sleep 5;
+# wait for systemui and increase its priority
+while $bb sleep 1; do
+  if [ `$bb pidof com.android.systemui` ]; then
+    systemui=`pidof com.android.systemui`;
+    $bb renice -18 $systemui;
+    $bb echo -17 > /proc/$systemui/oom_adj;
+    $bb chmod 100 /proc/$systemui/oom_adj;
+    exit;
   fi;
-done;
+done&
 
-# lmk whitelist for common launchers
-list="com.android.launcher org.adw.launcher org.adwfreak.launcher com.anddoes.launcher com.gau.go.launcherex com.mobint.hololauncher com.mobint.hololauncher.hd com.teslacoilsw.launcher com.cyanogenmod.trebuchet org.zeam";
-$bb sleep 10;
-for class in $list; do
-  pid=`pidof $class`;
-  if [ $pid != "" ]; then
-    echo "-17" > /proc/$pid/oom_adj;
-    chmod 100 /proc/$pid/oom_adj;
-  fi;
-done;
+# lmk whitelist for common launchers and increase launcher priority
+list="com.android.launcher org.adw.launcher org.adwfreak.launcher com.anddoes.launcher com.android.lmt com.chrislacy.actionlauncher.pro com.cyanogenmod.trebuchet com.gau.go.launcherex com.mobint.hololauncher com.mobint.hololauncher.hd com.teslacoilsw.launcher com.tsf.shell org.zeam";
+while $bb sleep 60; do
+  for class in $list; do
+    if [ `$bb pgrep $class` ]; then
+      launcher=`$bb pgrep $class`;
+      $bb echo -17 > /proc/$launcher/oom_adj;
+      $bb chmod 100 /proc/$launcher/oom_adj;
+      $bb renice -18 $launcher;
+    fi;
+  done;
+  exit;
+done&
